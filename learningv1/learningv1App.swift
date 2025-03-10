0c23e289f1c9d60f3bf30c071dbb9c0c3a03fdfc
Furqan Ali <furqan.ali9500@gmail.com>
2025-03-10 23:50:50 +0900

    Initial Commit

---
+++ learningv1/learningv1App.swift
@@ -0,0 +1,17 @@
+//
+//  learningv1App.swift
+//  learningv1
+//
+//  Created by Furqan Ali on 3/10/25.
+//
+
+import SwiftUI
+
+@main
+struct learningv1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learningv1Tests/learningv1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  learningv1Tests.swift
+//  learningv1Tests
+//
+//  Created by Furqan Ali on 3/10/25.
+//
+
+import Testing
+@testable import learningv1
+
+struct learningv1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
